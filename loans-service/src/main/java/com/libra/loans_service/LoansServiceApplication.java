e236447b405ff98c3b4b7999b4fbbee52b19ff5a
david priyadi <davidpriyadi77@gmail.com>
2024-07-19 17:42:24 +0700

    Update LoansServiceApplication.java

--- loans-service/src/main/java/com/libra/loans_service/LoansServiceApplication.java
+++ loans-service/src/main/java/com/libra/loans_service/LoansServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableWebMvc
 public class LoansServiceApplication {
 
 	public static void main(String[] args) {
